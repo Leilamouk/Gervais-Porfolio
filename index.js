308efe28749c07720b6c4033c26e5a8fb23bec32
Leilamouk <leilamoukagni@gmail.com>
2022-08-22 10:16:49 +0100

    modif

--- index.js
+++ index.js
@@ -5,17 +5,3 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
 }
-function sendEmail(){
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "ambourouetgervais12@gmail.com",
-        Password : "kinny03",
-        To : 'ambourouetgervais12@gmail.com',
-        From : document.getElementById("email") .value,
-        Subject : "New Contact Form Enquiry",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-    
-}
